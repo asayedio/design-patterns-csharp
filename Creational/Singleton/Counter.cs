1c99c0ae207bfb5cd140e5213c5160f398b6aacd
Ahmed Sayed <ahmedsayed.9191@gmail.com>
2022-07-19 00:09:15 +0200

    implement singleton pattern with single thread

---
+++ Creational/Singleton/Counter.cs
@@ -0,0 +1,17 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    public class Counter
+    {
+        public int count = 0;
+        private static Counter instance = null;
+
+        private Counter() { }
+
+        public static Counter GetInstance()
+        {
+            if (instance is null) instance = new Counter(); // Lazy Initialization
+            return instance;
+        }
+        public void AddOne() => count++;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/** Creational -> Singleton **/
+// Thread unSafety (Single thread)
+using DesignPatterns.Creational.Singleton;
+
+Counter counter1 = Counter.GetInstance();
+counter1.AddOne();
+counter1.AddOne();
+
+Counter counter2 = Counter.GetInstance();
+counter2.AddOne();
+counter2.AddOne();
+
+
+Console.WriteLine(counter1.count.ToString());
